5542c0f51f7429dbb59f615b32b1bab7420f8654
David <dball387@gmail.com>
2017-08-20 15:06:46 +0100

    Misc tidy up in keymap, update and add diagrams to readme

--- keymap.c
+++ keymap.c
@@ -14,6 +14,8 @@
     Some workaround is required to get a £ sign. :/
 */
 
+/* NB Keys noted with * in the diagrams are transparent: The mapping is derived from the layer below */
+
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
@@ -28,10 +30,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   ESC  |   1  |   2  |   3  |   4  |   5  | left |           |right |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   '  |   ,  |   .  |   p  |   y  |  TG  |           |      |   f  |   g  |   c  |   r  |   l  |   \    |
+ * |   DEL  |   '  |   ,  |   .  |   p  |   y  |  TG  |           |      |   f  |   g  |   c  |   r  |   l  |   \    |
  * |--------+------+------+------+------+------|MEDIA |           |      |------+------+------+------+------+--------|
- * |        |   a  |   o  |   e  |   u  |   i  |------|           |------|   d  |   h  |   t  |   n  |   s  |   /    |
- * |--------+------+------+------+------+------|  TG  |           |      |------+------+------+------+------+--------|
+ * |   ESC  |   a  |   o  |   e  |   u  |   i  |------|           |------|   d  |   h  |   t  |   n  |   s  |   /    |
+ * |--------+------+------+------+------+------|  TO  |           |      |------+------+------+------+------+--------|
  * | LSHIFT |   ;  |   q  |   j  |   k  |   x  |QWERTY|           |      |   b  |   m  |   w  |   v  |   z  | RSHIFT |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LCTRL| LGUI | ALT  |      |  L2  |                                       |  L2  |  LFT |  DWN |  UP  | RGHT |
@@ -73,72 +75,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* 1: QWERTY layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ESC  |   1  |   2  |   3  |   4  |   5  | left |           |right |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   ESC* |   1  |   2  |   3  |   4  |   5  | left |           |right |   6  |   7  |   8  |   9  |   0  |   -*   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   q  |   w  |   e  |   r  |   t  |  TG  |           |      |   y  |   u  |   i  |   o  |   p  |        |
+ * |   DEL* |   q  |   w  |   e  |   r  |   t  |  TG  |           |      |   y  |   u  |   i  |   o  |   p  |   \*   |
  * |--------+------+------+------+------+------|MEDIA |           |      |------+------+------+------+------+--------|
- * |        |   a  |   s  |   d  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  |   ;  |        |
- * |--------+------+------+------+------+------|  TG  |           |      |------+------+------+------+------+--------|
- * | LSHIFT |   z  |   x  |   c  |   v  |      |MEDIA |           |   b  |   n  |   m  |   ,  |   .  |   /  |        |
+ * |   ESC* |   a  |   s  |   d  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  |   ;  |   / *  |
+ * |--------+------+------+------+------+------|  TO  |           |      |------+------+------+------+------+--------|
+ * | LSHIFT*|   z  |   x  |   c  |   v  |   b  | BASE |           |      |   n  |   m  |   ,  |   .  |   /  | RSHIFT*|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | CTRL |  GUI | ALT  |      |  L2  |                                       |  L2  |  LFT |  DWN |  UP  | RGHT |
+ *   | CTRL* | GUI* | ALT*|      |  L2  |                                       |  L2  | LFT* | DWN* |  UP* | RGHT*|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 | SPC  | BSPC |------|       |------| TAB  | ENTR |
- *                                 |      |      |      |       |      |      |      |
+ *                                 | SPC* | BSPC*|------|       |------| TAB* | ENTR*|
+ *                                 |      |      | HOME*|       | END* |      |      |
  *                                 `--------------------'       `--------------------'
  */
   [QWERTY] = KEYMAP(
     /* LEFT HAND */
-    KC_ESCAPE,  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_LEFT,
-    KC_DELETE,  KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       TG(MEDIA),
-    KC_ESCAPE,  KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
-    KC_LSHIFT,  KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       TO(BASE),
-    KC_LCTL,    KC_LGUI,    KC_LALT,    KC_TRNS,    MO(SYMB),
+    KC_TRNS,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_LEFT,
+    KC_TRNS,    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       TG(MEDIA),
+    KC_TRNS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
+    KC_TRNS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       TO(BASE),
+    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    MO(SYMB),
 
                                                     KC_TRNS,    KC_TRNS,
                                                                 KC_TRNS,
-                                        KC_SPACE,   KC_BSPACE,  KC_TRNS,
+                                        KC_TRNS,    KC_TRNS,    KC_TRNS,
 
     /* RIGHT HAND */
     KC_RIGHT,   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_TRNS,
     KC_TRNS,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_TRNS,
                 KC_H,       KC_J,       KC_K,       KC_L,       KC_SCOLON,  KC_TRNS,
     KC_TRNS,    KC_B,       KC_N,       KC_M,       KC_COMMA,   KC_DOT,     KC_TRNS,
-    MO(SYMB),      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
+    MO(SYMB),   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 
                 KC_TRNS,    KC_TRNS,
                 KC_TRNS,
-                KC_TRNS,    KC_TAB,     KC_ENTER),
+                KC_TRNS,    KC_TRNS,     KC_TRNS),
 
 /* 2: Symbol layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ESC   |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           |  F7  |  F8  |  F9  | F10  | F11  |  F12 |        |
+ * |   ESC* |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           |  F7  |  F8  |  F9  | F10  | F11  |  F12 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   +  |   7  |   8  |   9  |   *  |        |
+ * |   DEL* |   !  |   @  |   {  |   }  |   |  |      |           |      |   +  |   7  |   8  |   9  |   *  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------|   =  |   4  |   5  |   6  |   0  |        |
+ * |   ESC* |   #  |   $  |   (  |   )  |   `  |------|           |------|   =  |   4  |   5  |   6  |   0  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   -  |   1  |   2  |   3  |   \  |        |
+ * | LSHIFT*|   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   -  |   1  |   2  |   3  |   \  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |    . |   0  |      |      |
+ *   | CTRL* | GUI* | ALT*|      |  L2  |                                       |  L2  | LFT* | DWN* |  UP* | RGHT*|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
+ *                                 | SPC* | BSPC*|------|       |------| TAB* | ENTR*|
+ *                                 |      |      | HOME*|       | END* |      |      |
  *                                 `--------------------'       `--------------------'
  */
 
   [SYMB] = KEYMAP(
     /* LEFT HAND */
-    KC_ESCAPE,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,
+    KC_TRNS,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,
     KC_TRNS,    KC_EXLM,    KC_AT,      KC_LCBR,    KC_RCBR,    KC_PIPE,    KC_TRNS,
     KC_TRNS,    KC_HASH,    KC_DLR,     KC_LPRN,    KC_RPRN,    KC_GRAVE,
     KC_TRNS,    KC_PERC,    KC_CIRC,    KC_LBRACKET,KC_RBRACKET,KC_TILD,    KC_TRNS,
@@ -146,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                     KC_TRNS,    KC_TRNS,
                                                                 KC_TRNS,
-                                        KC_SPACE,   KC_BSPACE,  KC_TRNS,
+                                        KC_TRNS,    KC_TRNS,    KC_TRNS,
 
     /* RIGHT HAND */
     KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,
@@ -157,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                 KC_TRNS,    KC_TRNS,
                 KC_TRNS,
-                KC_TRNS,    KC_TAB,     KC_ENTER),
+                KC_TRNS,    KC_TRNS,    KC_TRNS),
 
 /* 3: Media and motions layer
  *
--- readme.md
+++ readme.md
@@ -1,8 +1,5 @@
 # ErgoDox "ErgoDave"/"ErgoDvor" Configuration
 
-TODO
-- Finish writing
-- add diagrams
 ## Motivation
 
 Attempting to create an effective Dvorak-based, ErgoDox layout, largely for programming using (Neo)Vim in Linux environment.
@@ -16,20 +13,113 @@ Note: This layout is designed to be used with a US software layout.
 
 ### Base Layer (Dvorak)
 
+<pre><code>
+
+,--------------------------------------------------.           ,--------------------------------------------------.
+|   ESC  |   1  |   2  |   3  |   4  |   5  | left |           |right |   6  |   7  |   8  |   9  |   0  |   -    |
+|--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+|   DEL  |   '  |   ,  |   .  |   p  |   y  |  TG  |           |      |   f  |   g  |   c  |   r  |   l  |   \    |
+|--------+------+------+------+------+------|MEDIA |           |      |------+------+------+------+------+--------|
+|   ESC  |   a  |   o  |   e  |   u  |   i  |------|           |------|   d  |   h  |   t  |   n  |   s  |   /    |
+|--------+------+------+------+------+------|  TO  |           |      |------+------+------+------+------+--------|
+| LSHIFT |   ;  |   q  |   j  |   k  |   x  |QWERTY|           |      |   b  |   m  |   w  |   v  |   z  | RSHIFT |
+`--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+  | LCTRL| LGUI | ALT  |      |  L2  |                                       |  L2  |  LFT |  DWN |  UP  | RGHT |
+  `----------------------------------'                                       `----------------------------------'
+                                       ,-------------.       ,-------------.
+                                       |      |      |       |      |      |
+                                ,------|------|------|       |------+------+------.
+                                |      |      |      |       |      |      |      |
+                                | SPC  | BSPC |------|       |------| TAB  | ENTR |
+                                |      |      |      |       |      |      |      |
+                                `--------------------'       `--------------------'
+
+</pre></code>
+
 * **Layer keys** Layer keys to switch to the symbol layer are provided under each thumb. Other layer keys are provided but are not convenient as these will not be commonly used.
 * **Special chars on the right-most column.** I only use Ctrl & Shift keys on the left side, so I used the right keys to include some of the more useful special characters as well.
 * **No "speciality" keys (tap/hold differentiation)** Honestly, I figure that this is more hassle than it is worth.
 * **Common symbol keys** Some symbol common symbol keys are provided in the base layer (other than the standard .,'; set. I figured /\- are valuable to have on the base layer for typing paths and such.
 
 ### Secondary Typing layer (QWERTY)
+
+<pre><code>
+
+,--------------------------------------------------.           ,--------------------------------------------------.
+|   ESC* |   1  |   2  |   3  |   4  |   5  | left |           |right |   6  |   7  |   8  |   9  |   0  |   -*   |
+|--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+|   DEL* |   q  |   w  |   e  |   r  |   t  |  TG  |           |      |   y  |   u  |   i  |   o  |   p  |   \*   |
+|--------+------+------+------+------+------|MEDIA |           |      |------+------+------+------+------+--------|
+|   ESC* |   a  |   s  |   d  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  |   ;  |   / *  |
+|--------+------+------+------+------+------|  TO  |           |      |------+------+------+------+------+--------|
+| LSHIFT*|   z  |   x  |   c  |   v  |   b  | BASE |           |      |   n  |   m  |   ,  |   .  |   /  | RSHIFT*|
+`--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+  | CTRL* | GUI* | ALT*|      |  L2  |                                       |  L2  | LFT* | DWN* |  UP* | RGHT*|
+  `----------------------------------'                                       `----------------------------------'
+                                       ,-------------.       ,-------------.
+                                       |      |      |       |      |      |
+                                ,------|------|------|       |------+------+------.
+                                |      |      |      |       |      |      |      |
+                                | SPC* | BSPC*|------|       |------| TAB* | ENTR*|
+                                |      |      | HOME*|       | END* |      |      |
+                                `--------------------'       `--------------------'
+
+</pre></code>
+
 * A QWERTY layer is provided because reasons... Thought it could be handy to practice properly touch typing both Dvorak and QWERTY. It may also be useful for others who want to try it out :) This layer is functionally the same as the Dvorak layer in terms of the layer switching and usage.
 
 ### Symbol layer
+
+<pre><code>
+
+,--------------------------------------------------.           ,--------------------------------------------------.
+|   ESC* |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           |  F7  |  F8  |  F9  | F10  | F11  |  F12 |        |
+|--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+|   DEL* |   !  |   @  |   {  |   }  |   |  |      |           |      |   +  |   7  |   8  |   9  |   *  |        |
+|--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+|   ESC* |   #  |   $  |   (  |   )  |   `  |------|           |------|   =  |   4  |   5  |   6  |   0  |        |
+|--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+| LSHIFT*|   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   -  |   1  |   2  |   3  |   \  |        |
+`--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+  | CTRL* | GUI* | ALT*|      |  L2  |                                       |  L2  | LFT* | DWN* |  UP* | RGHT*|
+  `----------------------------------'                                       `----------------------------------'
+                                       ,-------------.       ,-------------.
+                                       |      |      |       |      |      |
+                                ,------|------|------|       |------+------+------.
+                                |      |      |      |       |      |      |      |
+                                | SPC* | BSPC*|------|       |------| TAB* | ENTR*|
+                                |      |      | HOME*|       | END* |      |      |
+                                `--------------------'       `--------------------'
+
+</pre></code>
 
 * **Brackets and Braces** These are all provided explicitly (without shifting) and arranged in-line.
 * **Other symbols** All symbols should be present somewhere on this layer, mostly in common typing positions.
 
 ### Media and Motions layer
+
+<pre><code>
+
+,--------------------------------------------------.           ,--------------------------------------------------.
+|        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+|-------- ------ ------ ------ ------ -------------|           |------ ------ ------ ------ ------ ------ --------|
+|        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+|-------- ------ ------ ------ ------ ------|      |           |      |------ ------ ------ ------ ------ --------|
+|        | MLFT | MDWN | MUP  | MRGT |      |------|           |------|      |      |      |      |      |        |
+|-------- ------ ------ ------ ------ ------|      |           |      |------ ------ ------ ------ ------ --------|
+|        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+`--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+  |      |      |      |      |      |                                       |      |      |      |      |      |
+  `----------------------------------'                                       `----------------------------------'
+                                       ,-------------.       ,-------------.
+                                       |      |      |       |      |      |
+                                ,------|------|------|       |------+------+------.
+                                |  M   |  M   |      |       |      |      |      |
+                                | LFT  | RGT  |------|       |------|      |      |
+                                | CLK  | CLK  |      |       |      |      |      |
+                                `--------------------'       `--------------------'
+
+</pre></code>
 
 * **Scope for many media (and other) control keys** Currently none of these keys are added, at least not until I figure out what these should look like. In any case, an entire layer is dedicated to the potential of providing this functionality. If this layer is useful, it will be worth making this layer more easily accessible.
 * Provided mouse keys although I do not fully intend to use them.
@@ -38,7 +128,7 @@ Note: This layout is designed to be used with a US software layout.
 
 * This layout should be built as part of the [QMK firmware](https://github.com/qmk/qmk_firmware) repository.
 
-Clone the firmware repo and plant this layout into its keymappings:
+Clone the firmware repo and plant this layout into its ergodox keymappings:
 ```bash
 $ git clone https://github.com/qmk/qmk_firmware
 $ cd qmk_firmware
